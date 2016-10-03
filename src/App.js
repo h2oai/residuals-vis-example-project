3519c1a427b09f5a701001585b2d2eb1ad05b4f1
Micah Stubbs <micah.stubbs@gmail.com>
2016-10-03 15:58:11 -0700

    add modelIDs for grupoBimboConfig

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { ResidualsVis } from 'residualsVis';
 
+// import { rossmanAggregatedConfig } from './config/rossmanAggregated';
+import { santanderAggregatedConfig } from './config/santanderAggregated';
+// import { grupoBimboAggregatedConfig } from './config/grupoBimboAggregated';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ResidualsVis/>
+        <ResidualsVis config={santanderAggregatedConfig}/>
       </div>
     );
   }
