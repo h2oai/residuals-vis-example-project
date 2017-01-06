5f8f4a076e862e9b645d5c7b4c19d238beea2693
Micah Stubbs <micah.stubbs@gmail.com>
2017-01-05 18:22:36 -0800

    make the GBEngFeatures component the page that renders at the root

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import App from './App';
-import Home from './Home';
-import GBRawFeatures from './GBRawFeatures';
+// import Home from './Home';
+// import GBRawFeatures from './GBRawFeatures';
 import GBEngFeatures from './GBEngFeatures';
-import Wine from './Wine';
-import Iris from './Iris';
+// import Wine from './Wine';
+// import Iris from './Iris';
 import './index.css';
 import './bootstrap.min.css';
 
 render(( 
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="/gb-raw-features" component={GBRawFeatures}/>
-      <Route path="/gb-eng-features" component={GBEngFeatures}/>
-      <Route path="/wine" component={Wine}/>
-      <Route path="/iris" component={Iris}/>
+      <IndexRoute component={GBEngFeatures}/>
+      {/* <Route path="/gb-raw-features" component={GBRawFeatures}/> */}
+      {/* <Route path="/gb-eng-features" component={GBEngFeatures}/> */}
+      {/* <Route path="/wine" component={Wine}/> */ }
+      {/* <Route path="/iris" component={Iris}/> */ }
     </Route>
   </Router>
 ), document.getElementById('root'))
